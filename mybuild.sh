068f74e6df2b45156f5f22e011c3b915ba1caefa
Your Name <you@example.com>
2016-12-05 01:31:23 +0900

    miod build

--- mybuild.sh
+++ mybuild.sh
@@ -15,7 +15,7 @@ javac $BASEDIR/WEB-INF/src/*.java -classpath /usr/lib/tomcat/lib/servlet-api.jar
 
 rm -rf $BASEDIR/WEB-INF/classes
 mkdir $BASEDIR/WEB-INF/classes
-mv $BASEDIR/WEB-INF/*.class $BASEDIR/WEB-INF/classes
+mv $BASEDIR/WEB-INF/src/*.class $BASEDIR/WEB-INF/classes
 
 $TOMCAT_HOME/bin/shutdown.sh
 $TOMCAT_HOME/bin/startup.sh
